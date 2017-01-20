b7801f7a89cd8b5a9431920a92a2a57158261523
Lusine <lusin.khachatryan@mail.ru>
2017-01-20 23:39:26 +0400

    com

---
+++ Passwordgeneratortest/Passwordgenerator/Randompasswordgenerator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace Passwordgenerator
+{
+    public static class Passwordgen
+    {
+
+
+
+        public static string NewPindigits(int length, int dig)
+        {
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[length];
+                rng.GetBytes(data);
+                var seed = BitConverter.ToInt32(data, 0);
+
+                var rand = new Random(seed);
+
+                Random rnd = new Random();
+
+                string password = String.Empty;
+
+
+
+
+                if (dig > 5 || dig < 1)
+                {
+
+
+
+
+                    password = "incorrect enter:  dig";
+                    return password;
+                }
+
+                else
+
+
+                    if (dig == 1)
+                    {
+                        for (int j = 0; j < length; j++)
+                        {
+
+
+
+                            password += (char)rand.Next(97, 122);
+
+
+
+                        }
+                    }
+                    else
+
+                        if (dig == 2)
+                        {
+                            {
+                                for (int j = 0; j < length; j++)
+                                {
+
+
+
+                                    password += (char)rand.Next(48, 57);
+
+
+
+                                }
+                            }
+                        }
+
+
+
+
+                        else
+
+                            if (dig == 3)
+                            {
+                                {
+                                    for (int j = 0; j < length; j++)
+                                    {
+
+
+
+                                        password += (char)rand.Next(65, 90);
+
+
+
+                                    }
+                                }
+                            }
+
+
+
+                            else
+
+
+
+                                if (dig == 4)
+                                {
+                                    {
+
+
+                                        for (int j = 0; j < length; j++)
+                                        {
+
+                                            if (rnd.Next(1, 5) == 1)
+                                                password += (char)rnd.Next(65, 90);
+
+                                            else if (rnd.Next(1, 5) == 2)
+                                                password += (char)rnd.Next(48, 57);
+                                            else
+                                                if (rnd.Next(1, 5) == 3)
+                                                    password += (char)rnd.Next(48, 57);
+                                                else
+                                                    if (rnd.Next(1, 5) == 4)
+                                                        password += (char)rnd.Next(97, 122);
+
+
+
+
+                                        }
+                                    }
+                                }
+
+
+
+                return password;
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Passwordgeneratortest/Passwordgeneratortest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Passwordgenerator;
+
+namespace Passwordgeneratortest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int dig = int.Parse(Console.ReadLine());
+            //length is the length of the password 
+            //if the dig = 1, then  generated password consisting of numbers
+            //if the dig = 2, then password generated only of the symbols
+            //if the dig = 3, then  generated password consisting of letters
+            // if the dig = 4 , password is made up of symbols, letters and numbers.
+
+            Console.WriteLine(Passwordgen.NewPindigits(length, dig));
+        }
+    }
+}
